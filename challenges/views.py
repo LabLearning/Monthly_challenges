aadc1cb4591355f03b3552b797accb5ae0122863
HustleCoding <f.dobinciuc7@gmail.com>
2022-05-13 17:42:22 +0300

    Add a new url for february slug

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def index(request):
-	return HttpResponse("This works")
+    return HttpResponse("This works")
+
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes every day!")
